3436c553c82dc5d03ce0acd1b135fe7a1fc87b9a
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-05-22 02:07:57 +0200

    fine 2/2 error Button "Calculate"

--- text.js
+++ text.js
@@ -1,26 +1,61 @@
 let button = document.getElementById('calculate');
 
+
+
 button.addEventListener('click' , function(){
     let km = parseInt(document.getElementById('km').value);
     let age = parseInt(document.getElementById('age').value);
+    let name = document.getElementById('name').value;
 
+    let discount_perc;
     let price = km * 0.21;
-    let message = '';
+  
 
+
+if(isNaN(km) === false){
     if(age === 0){
-        message += `il biglietto costa: ${price}€`;
+        discount_perc = '/';
+        // message += `il biglietto costa: ${price}€`;
     }
 
     else if (age === 1) {
-        price = price * 0.8;
-        message += `il prezzo del biglietto è di ${price}€`;
+        discount_perc = '20%';
+        price *= 0.8;
+        
+        // message += `il prezzo del biglietto è di ${price.toFixed}€`;
     }
 
     else {
-        price = price - price * 0.4; 
-        message += `il prezzo del biglietto è di ${price}€`;
+        discount_perc = '40%';
+        price *= 0.6; 
+        // message += `il prezzo del biglietto è di ${price.toFixed}€`;
 
+        }
+
+        document.getElementById('show-name').innerText = name;
+        document.getElementById('show-km').innerText = km;
+        document.getElementById('show-discount').innerText = discount_perc;
+        document.getElementById('show-price').innerText = price.toFixed(2)+'€';
+
+        document.querySelector('.show-error').innerText = '';
+    }
+    else {
+        document.querySelector('.show-error').innerText = 'inserisci km da percorrere';
     }
 
-    document.getElementById('price').innerText = message;
+   
+    
 });
+
+document.getElementById('reset').addEventListener('click' , function() {
+    document.getElementById('km').value = '';
+    document.getElementById('name').value = '';
+
+    document.getElementById('show-name').innerText = '';
+    document.getElementById('show-km').innerText = '';
+    document.getElementById('show-discount').innerText = '';
+    document.getElementById('show-price').innerText = '';
+
+    document.querySelector('.show-error').innerText = '';
+    
+} )
